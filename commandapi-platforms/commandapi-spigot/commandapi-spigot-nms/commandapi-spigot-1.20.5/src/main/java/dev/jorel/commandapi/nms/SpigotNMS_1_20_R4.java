f0b1f9a08877a22ee62e4972950bbfa7c1058b7d
DerEchtePilz <81232921+DerEchtePilz@users.noreply.github.com>
2024-06-06 19:56:04 +0200

    Move PaperCommandRegistration to commandapi-paper-core

--- commandapi-platforms/commandapi-spigot/commandapi-spigot-nms/commandapi-spigot-1.20.5/src/main/java/dev/jorel/commandapi/nms/SpigotNMS_1_20_R4.java
+++ commandapi-platforms/commandapi-spigot/commandapi-spigot-nms/commandapi-spigot-1.20.5/src/main/java/dev/jorel/commandapi/nms/SpigotNMS_1_20_R4.java
@@ -2,10 +2,8 @@ package dev.jorel.commandapi.nms;
 
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.tree.CommandNode;
 import dev.jorel.commandapi.CommandAPIBukkit;
 import dev.jorel.commandapi.CommandRegistrationStrategy;
-import dev.jorel.commandapi.PaperCommandRegistration;
 import dev.jorel.commandapi.SpigotCommandRegistration;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
@@ -16,7 +14,6 @@ import net.minecraft.commands.arguments.MessageArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.craftbukkit.v1_20_R4.CraftServer;
 import org.bukkit.craftbukkit.v1_20_R4.command.BukkitCommandWrapper;
