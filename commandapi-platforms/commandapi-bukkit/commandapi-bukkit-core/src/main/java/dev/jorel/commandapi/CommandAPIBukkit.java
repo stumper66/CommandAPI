83ee2cfcb786fd4a885c7e0932737e82c17d17f6
BuildTools <46540330+willkroboth@users.noreply.github.com>
2024-06-20 19:34:51 +0200

    Implement `DynamicMultiLiteralArgument`
    Acts like `MultiLiteralArgument`, but literals can be changed and differ for each CommandSender
    
    Resolves https://github.com/JorelAli/CommandAPI/issues/513
    
    Uses Paper's `AsyncPlayerSendCommandsEvent` and Velocity's `PlayerAvailableCommandsEvent` to change the client's view of the command tree. Note that a similar event does not exist on Spigot, so the suggestions are not dynamic, though the parsing still is.
    
    `DifferentClientNode` class added to handle creating client-server command tree de-syncs (I believe this can be used to make `FlagsArgument` platform-agnostic)

--- commandapi-platforms/commandapi-bukkit/commandapi-bukkit-core/src/main/java/dev/jorel/commandapi/CommandAPIBukkit.java
+++ commandapi-platforms/commandapi-bukkit/commandapi-bukkit-core/src/main/java/dev/jorel/commandapi/CommandAPIBukkit.java
@@ -181,9 +181,9 @@ public abstract class CommandAPIBukkit<Source> implements CommandAPIPlatform<Arg
 			public void onServerLoad(ServerLoadEvent event) {
 				CommandAPI.stopCommandRegistration();
 			}
-		}, getConfiguration().getPlugin());
+		}, plugin);
 
-		paper.registerReloadHandler(plugin);
+		paper.registerEvents(plugin);
 	}
 
 	/*
